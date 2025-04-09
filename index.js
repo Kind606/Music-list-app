f6a2416880213d68905e857d524e21bf5db5a5df
Kind606 <kevin.hellgren@hotmail.com>
2025-04-09 09:51:02 +0200

    Appended cards from json server

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
+import http from "./scripts/httpClient.js";
 
-import http from './scripts/httpClient.js';
-
-const musicList = querySelector("#gallary")
+const musicList = document.querySelector("#music");
 
 const initApp = () => {
-    if (location.href.endsWith('gallery.html')) {
-      loadMusic();
-    } else if (location.href.includes('favmusic.html')) {
-      loadFavMusic();
-    }
-  };
+    loadMusic();
+};
 
-  const loadMusic = async () => {
-    const client = new http('music');
-    const songs = await client.get();
-  
-    console.log(songs);
-  
-    // for (let song of songs) {
-    //   const section = document.createElement('section');
-    //   section.classList.add('card');
-  
-    //   const image = document.createElement('img');
-    //   image.setAttribute('src', `./images/${vehicle.imageUrl}`);
-    //   image.alt = vehicle.manufacturer;
-    //   image.setAttribute('vehicleId', vehicle.id);
-  
-    //   section.appendChild(image);
-  
-    //   const info = document.createElement('p');
-    //   info.innerText = vehicle.manufacturer;
-  
-    //   section.appendChild(info);
-  
-    //   vehicleList.appendChild(section);
-    // }
-  
-    // const images = document.querySelectorAll('img');
-    // images.forEach((image) => {
-    //   image.addEventListener('click', () => {
-    //     console.log(`Du klickade på ${image.getAttribute('vehicleId')}`);
-    //     location.href =
-    //       './vehicle-details.html?vehicle=' + image.getAttribute('vehicleId');
-    //   });
-    // });
-  };
+const loadMusic = async () => {
+  const client = new http("songs");
+  const songs = await client.get();
 
+  console.log(songs);
+
+  for (let song of songs) {
+    const section = document.createElement("section");
+    section.classList.add("card");
+
+    const image = document.createElement("img");
+    image.setAttribute("src", `./images/${song.image}`);
+    image.alt = songs.title;
+    image.setAttribute("songId", song.id);
+
+    section.appendChild(image);
+
+    const info = document.createElement("p");
+    info.innerText = song.artist;
+
+    section.appendChild(info);
+
+    musicList.appendChild(section);
+  }
+
+  //   const images = document.querySelectorAll('img');
+  //   images.forEach((image) => {
+  //     image.addEventListener('click', () => {
+  //       console.log(`Du klickade på ${image.getAttribute('vehicleId')}`);
+  //       location.href =
+  //         './vehicle-details.html?vehicle=' + image.getAttribute('vehicleId');
+  //     });
+  //   });
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
